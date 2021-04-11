b799830eb6764b2b4ec5c07cdb3dbdaab95d465e
Md. Asif Karim Chayon <mchayon201366@bscse.uiu.ac.bd>
2021-04-11 13:51:05 +0600

    A to Z using for loop

---
+++ Array.java
@@ -0,0 +1,21 @@
+public class Array {
+    public static void main(String[] args) {
+        int[] arr1 = new int[10];
+        float[] arr2 = new float[10];
+        //printing integer number
+        for (int i = 0; i < 10; i++) {
+            arr1[i] = 1 + i;
+
+            System.out.println(arr1[i]);//by using println no need to add new line ,it's by default
+        }
+        System.out.println("\n");
+        //printing floating point number
+        for (int j = 0; j < 10; j++) {
+            arr2[j] = 1 + j;
+            System.out.print(arr2[j]+" ");//print uses no new line by default
+        }
+        System.out.println("\n");
+        
+    }
+
+}
---
+++ AtoZ.java
@@ -0,0 +1,12 @@
+public class AtoZ {
+    public static void main(String[] args){
+        for (char c='A';c<='Z';c++){//same for small latters
+           
+            System.out.println(c);
+        }
+        System.out.println("-----------------");
+        for (char c='a';c<='z';c++){           
+            System.out.println(c);
+        }
+    }
+}
