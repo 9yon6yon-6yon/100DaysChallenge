03e9d42d941ed627418e0676605aa5dd6423eba6
Md. Asif Karim Chayon <mchayon201366@bscse.uiu.ac.bd>
2021-04-12 19:24:46 +0600

    Commit

---
+++ mathpackage/TicTacToeGame.java
@@ -0,0 +1,22 @@
+package mathpackage;
+
+import java.util.*;
+
+
+class Game {
+    char[] name = new char[50];
+    char[] name2 = new char[50];
+    int[][] data = new int[3][3];
+    int i, j, sign, player, flag;
+    char sp;
+}
+
+public class TicTacToeGame {
+
+    public static void Main(String[] args) {
+
+        
+        // TicTacToeGame game = new TicTacToeGame();
+
+    }
+}
