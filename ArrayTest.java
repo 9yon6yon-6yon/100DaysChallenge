9f09a104e688bc1dfcc043d09b16f9beba07621c
Md. Asif Karim Chayon <mchayon201366@bscse.uiu.ac.bd>
2021-04-15 11:34:24 +0600

    Array in java

---
+++ ArrayTest.java
@@ -0,0 +1,15 @@
+public class ArrayTest {
+    public static void main(String[] args){
+        int[] arr ;//declaring array
+        arr = new int[5];//creating array
+        int i;
+        for(i=0;i<5;i++)
+            arr[i]= 100+i;//initializing array
+        for(i=0;i<5;i++)
+        System.out.println(arr[i]);
+        
+
+
+    }
+    
+}
