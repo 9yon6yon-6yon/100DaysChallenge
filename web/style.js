7a2eebf1cef5673225feecea6b47e09a71044bb0
Md. Asif Karim Chayon <mchayon201366@bscse.uiu.ac.bd>
2021-04-16 15:49:22 +0600

    WebPage Design

---
+++ web/style.js
@@ -0,0 +1,12 @@
+alert("Hi there");
+document.write("Hello world! \n");
+console.log("Hello from console...");
+var x =100;
+document.write(x);
+console.log(x);
+/*this is a comment*/
+//this is a comment
+var name="Chayon";
+var text="My name is Chayon";
+var sayhello='\n Hello world! \' I am a JavaScript programmer. \' ';
+document.write(sayhello);
