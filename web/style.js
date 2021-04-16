63192521d807411b96514aae169cfd14f2b8b551
Md. Asif Karim Chayon <mchayon201366@bscse.uiu.ac.bd>
2021-04-16 16:11:55 +0600

    JavaScript Basics

--- web/style.js
+++ web/style.js
@@ -1,14 +1,39 @@
 alert("Hi there");
 document.write("Hello world! \n");
 console.log("Hello from console...");
-var x =100;
-document.write(x);
-console.log(x);
+//var x =100;
+//document.write(x);
+//console.log(x);
 /*this is a comment*/
 //this is a comment
-var name="Chayon";
-var text="My name is Chayon";
-var sayhello='\n Hello world! \' I am a JavaScript programmer. \' ';
+var name = "Chayon";
+var text = "My name is Chayon";
+var sayhello = '\n Hello world! \' I am a JavaScript programmer. \' ';
 document.write(sayhello);
 var isActive = true;
 var isHoliday = false;
+var x = 6;
+
+if (x == 1) {
+    document.write('<h1>HTML5</h1>');
+}
+else if (x == 2) { document.write('<h1>CSS3</h1>'); }
+else { document.write('<h1>JavaScript</h1>'); }
+switch (x) {
+    case 1:
+        document.write("This is case 1 \n");
+        break;
+    case 2:
+        document.write("This is case 1 \n");
+        break;
+    case 3:
+        document.write("This is case 1 \n");
+        break;
+    default:
+        document.write("Hi I am a default case \n");
+}
+document.write("<br>");
+for(i=1;i<=10;i++){
+    document.write(i+"<br>");
+}
+
