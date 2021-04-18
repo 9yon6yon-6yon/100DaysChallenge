e2731f08bb75443ffc4019656df438dd00380e22
Md. Asif Karim Chayon <mchayon201366@bscse.uiu.ac.bd>
2021-04-18 19:16:27 +0600

    Memory Allocation

---
+++ Calloc.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*“calloc” or “contiguous allocation” method in C is used to dynamically allocate the specified number of 
+blocks of memory of the specified type. It initializes each block with a default value ‘0’.
+
+*/
+int main()
+{
+
+	// This pointer will hold the
+	// base address of the block created
+	int* ptr;
+	int n, i;
+
+	// Get the number of elements for the array
+	n = 5;
+	printf("Enter number of elements: %d\n", n);
+
+	// Dynamically allocate memory using calloc()
+	ptr = (int*)calloc(n, sizeof(int));
+
+	// Check if the memory has been successfully
+	// allocated by calloc or not
+	if (ptr == NULL) {
+		printf("Memory not allocated.\n");
+		exit(0);
+	}
+	else {
+
+		// Memory has been successfully allocated
+		printf("Memory successfully allocated using calloc.\n");
+
+		// Get the elements of the array
+		for (i = 0; i < n; ++i) {
+			ptr[i] = i + 1;
+		}
+
+		// Print the elements of the array
+		printf("The elements of the array are: ");
+		for (i = 0; i < n; ++i) {
+			printf("%d, ", ptr[i]);
+		}
+	}
+
+	return 0;
+}
