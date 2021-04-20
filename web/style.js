4c22e855add5d2174269c554c304591e07353960
Md. Asif Karim Chayon <mchayon201366@bscse.uiu.ac.bd>
2021-04-20 20:46:02 +0600

    Commit

--- web/style.js
+++ web/style.js
@@ -1,12 +1,14 @@
-alert("Hi there");
-document.write("Hello world! \n");
+//alert("Hi there");
+
+
+//document.write("Hello world! \n");
 console.log("Hello from console...");
 //var x =100;
 //document.write(x);
 //console.log(x);
 /*this is a comment*/
 //this is a comment
-var name = "Chayon";
+/*var name = "Chayon";
 var text = "My name is Chayon";
 var sayhello = '\n Hello world! \' I am a JavaScript programmer. \' ';
 document.write(sayhello);
@@ -33,7 +35,47 @@ switch (x) {
         document.write("Hi I am a default case \n");
 }
 document.write("<br>");
-for(i=1;i<=10;i++){
-    document.write(i+"<br>");
+for (i = 1; i <= 10; i++) {
+    document.write(i + "<br>");
+}*/
+document.write("<br>");
+var Height = document.documentElement.scrollHeight;
+var currentHeight = 0;
+var bool = true;
+var step = 4;
+var speed = 15;
+var interval = setInterval(scrollpage, speed)
+
+function scrollpage() {
+    if (currentHeight < 0 || currentHeight > Height)
+        bool = !bool;
+    if (bool) {
+        window.scrollTo(0, currentHeight += step);
+    } else {
+        //if you don't want to continue scroll 
+        // clearInterval(interval) use clearInterval
+        window.scrollTo(0, currentHeight += step);
+    }
+
 }
+
+scrollpage();
+
+var val = 1000000;
+while (val--) {
+    if (val % 2 == 1) {
+        i = 0;
+        j = 1;
+       
+        document.write(i + " " + j + " " );
+    }
+    else {
+        i = 1;
+        j = 0;
+       
+        document.write(i + " " + j + " ");
+    }
+  
+}
+
 
