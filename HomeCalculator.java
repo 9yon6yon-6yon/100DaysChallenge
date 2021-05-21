0b13b5a93d7d35d58d8e85c7913f723515e5a0e0
Md. Asif Karim Chayon <mchayon201366@bscse.uiu.ac.bd>
2021-05-21 12:20:01 +0600

    Calculator

--- HomeCalculator.java
+++ HomeCalculator.java
@@ -4,36 +4,80 @@ import java.awt.event.*;
 public class HomeCalculator implements WindowListener, ActionListener {
 
     private Frame f;
-    private Button b[] = new Button[10];
+
+    public Button b[] = new Button[10];
+
     int i;
-    private TextField tf;
-    private Button dot, plus, minus, equal, div, multi, clr;
+    public TextField tf;
+    public Button cut, br1, br2, dot, plus, minus, equal, div, multi, clr;
 
     public HomeCalculator() {
         f = new Frame("Basic Calculator");
-        tf = new TextField(10);
-       
+        tf = new TextField(30);
 
     }
 
-    private void launchFrame() {
-        tf.setForeground(Color.blue);
-        tf.setBackground(Color.white);
-        f.add(tf);
+    public void launchFrame() {
+
+        //f.add(tf, BorderLayout.NORTH);
+        f.setLayout(new GridLayout(5, 3));
+
         for (i = 0; i <= 9; i++) {
             b[i] = new Button(i + "");
 
             b[i].addActionListener(this);
-            b[i].setBackground(Color.black);
-            b[i].setForeground(Color.white);
-            f.add(b[i]);
 
         }
-        f.setLayout(new GridLayout(4,4));
-       
+        dot = new Button(".");
+        clr = new Button("C");
+        multi = new Button("*");
+        plus = new Button("+");
+        minus = new Button("-");
+        equal = new Button("=");
+        div = new Button("/");
+        br1 = new Button("(");
+        br2 = new Button(")");
+        cut = new Button("<-");
+        dot.addActionListener(this);
+        clr.addActionListener(this);
+        multi.addActionListener(this);
+        plus.addActionListener(this);
+        minus.addActionListener(this);
+        equal.addActionListener(this);
+        div.addActionListener(this);
+        br1.addActionListener(this);
+        br2.addActionListener(this);
+        cut.addActionListener(this);
+
+        f.add(br1);
+        f.add(br2);
+        f.add(cut);
+        f.add(clr);
+
+        f.add(b[7]);
+        f.add(b[8]);
+        f.add(b[9]);
+        f.add(div);
+
+        f.add(b[4]);
+        f.add(b[5]);
+        f.add(b[6]);
+        f.add(multi);
+
+        f.add(b[1]);
+        f.add(b[2]);
+        f.add(b[3]);
+        f.add(minus);
+
+        f.add(b[0]);
+        f.add(dot);
+        f.add(plus);
+        f.add(equal);
+
         f.addWindowListener(this);
-        f.setSize(250, 150);
+        f.setSize(250, 200);
         f.setBackground(Color.lightGray);
+
         f.setVisible(true);
     }
 
@@ -66,6 +110,7 @@ public class HomeCalculator implements WindowListener, ActionListener {
     public static void main(String[] args) {
         HomeCalculator hc = new HomeCalculator();
         hc.launchFrame();
+
     }
 
 }
