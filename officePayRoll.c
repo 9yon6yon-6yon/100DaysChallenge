e122ae6ba9b5afde9bcc7908d76445f10c9f920a
Md. Asif Karim Chayon <mchayon201366@bscse.uiu.ac.bd>
2021-06-03 21:06:43 +0600

    Structure

--- officePayRoll.c
+++ officePayRoll.c
@@ -109,3 +109,40 @@ int main()
     }
     return 0;
 }
+
+/*
+You will extend the assignment one. Here is the description.
+
+(a)(i) Create an Employee structure that will have the following members: name (string type), phoneNo (string type), and a salary (float type).
+
+(ii) In the main function, create an array of size N of Employee structure type. This array will be used by all the functions. You will pass this array and # of employees as arguments to the functions.
+
+(b) Create a function “takeEmployeesInfoFromKeyboard” that takes an Employee structure array and the size of the array as parameters. It takes information of all the Employees from keyboard and stores in the Employee structure array. Do you know that arrays are passed by reference?
+
+(c) Create a function “searchAnEmployee” that takes an Employee structure array, a phone number, and the size of the array as parameters. It finds and displays the employee’s information from the array searching by the phone number.
+
+(d) Create a function “displayAllEmployeesInfoReverse” that takes an Employee structure array and the size of the array as parameters. It displays all employees’ information in reverse index order in a tabular format with following values (including the given column heading):
+
+Employee  Name – Employee  Phone No - Salary
+
+         ?                          ?                              ?     
+
+(e) Write other necessary code to create the following menu system similar to assignment one. The following menu will always be displayed except if exited by pressing ‘q’.
+
+ 
+
+======= Office Pay Roll System =========
+
+• Enter ‘r, to take information of all the employee into the Employee structure array of size N.
+
+• Enter ‘s’, to search an employee by phone no. If found, display the info of employee. If not, display “Not Found”.
+
+• Enter ‘t’, to display the information of all the employees in reverse index order in a tabular format.
+
+• Enter ‘q’, quit/exit the menu system. 
+
+• For any other input, display “Invalid Input”
+
+ 
+
+After the menu is displayed, the program will prompt the user “Enter your choice: “ to enter a choice from the menu. After user enters his choice, using switch case the program will call appropriate functions to do the task.*/
