74d5b57f9c2f992136844a4be01a303127c7e5c3
Md. Asif Karim Chayon <mchayon201366@bscse.uiu.ac.bd>
2021-12-29 01:39:31 +0600

    Not necessary

--- URI.java
+++ URI.java
@@ -5,16 +5,20 @@ public class URI {
 
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int i, j;
-        for (i = 1; i <= n; i++) {
-            for (j = 1; j <= 2; j++) {
-                if (j == 1) {
-                    System.out.println(i + " " + (i * i) + " " + (i * i * i));
-                } else {
-                    System.out.println(i + " " + ((i * i) + 1) + " " + ((i * i * i) + 1));
-                }break;
+        boolean flag = true;
+        while (flag) {
+            int n = sc.nextInt();
+            if (n == 0)
+                flag = false;
+
+            for (int i = 1; i <= n; i++) {
+                for (int j = 0; j < i; j++) {
+                    System.out.print(i);
+                }
+                System.out.println();
+
             }
+
         }
 
     }
